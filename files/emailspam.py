76b0681a203c306944da13da2f89406a32b08cf1
lukecodingg <tatesempire2.0@gmail.com>
2025-05-31 10:41:29 +0200

    Update emailspam.py

--- files/emailspam.py
+++ files/emailspam.py
@@ -3,89 +3,80 @@ import sys
 import smtplib
 import getpass
 
-W = ''  # White
-R = ''  # Red
-G = ''  # Green
+G = '\033[92m'
+RESET = '\033[0m'
 
-menu = """ 
+os.system('cls' if os.name == 'nt' else 'clear')
+
+menu = f"""{G}
                                                                            
  _    _   _ _  _______   __  __ _   _ _   _____ ___ _____ ___   ___  _     
 | |  | | | | |/ | ____| |  \/  | | | | | |_   _|_ _|_   _/ _ \ / _ \| |    
 | |  | | | | ' /|  _|   | |\/| | | | | |   | |  | |  | || | | | | | | |    
 | |__| |_| | . \| |___  | |  | | |_| | |___| |  | |  | || |_| | |_| | |___ 
 |_____\___/|_|\_|_____| |_|  |_|\___/|_____|_| |___| |_| \___/ \___/|_____|
-
 ---------------------------------------------------------------------------
-
-By lukecodingg
+By lukecodingg for ebola 😻
+{RESET}
 """
 
 print(menu)
 
-if os.name == 'nt':  # Windows
-    os.system('cls')
-else:  # Linux/Unix/MacOS
-    os.system('clear')
+server_input = input(G + 'Mail Server (Gmail/Yahoo): ' + RESET).strip().lower()
 
-server = input('Mail-Server Gmail/Yahoo: ')
-
-if server.lower() == 'gmail':
-
+if server_input == 'gmail':
     smtp_server = 'smtp.gmail.com'
     port = 587
-    set_server = 'gmail'
-
-elif server.lower() == 'yahoo':
-
+elif server_input == 'yahoo':
     smtp_server = 'smtp.mail.yahoo.com'
-    port = 25
-    set_server = 'yahoo'
-
+    port = 587
 else:
-
-    print(R + 'Error - This script only works on Gmail or Yahoo.' + W)
+    print(G + 'Error - This script only works on Gmail or Yahoo.' + RESET)
     sys.exit()
 
-email_user = input('Email: ')
-passwd = getpass.getpass('Password: ')
-email_to = input('\nTo: ')
-subject = input('Subject: ')
-body = input('Message: ')
-total = int(input('Amount of Sendings: '))
+email_user = input(G + 'Email: ' + RESET)
+passwd = getpass.getpass(G + 'Password: ' + RESET)
+email_to = input(G + '\nTo: ' + RESET)
+subject = input(G + 'Subject: ' + RESET)
+body = input(G + 'Message: ' + RESET)
 
 try:
+    total = int(input(G + 'Amount of Sendings: ' + RESET))
+except ValueError:
+    print(G + 'Error - Please enter a valid number.' + RESET)
+    sys.exit()
 
+try:
     server = smtplib.SMTP(smtp_server, port)
     server.ehlo()
 
-    if set_server == 'gmail':
+    if port == 587:
         server.starttls()
 
     server.login(email_user, passwd)
 
-    print('\n\n\n - Target : {} -\n'.format(email_to))
+    print(G + f'\n\n\n - Target : {email_to} -\n' + RESET)
 
     for i in range(1, total + 1):
-
-        msg = 'From: ' + email_user + '\nSubject: ' + subject + '\n' + body
+        msg = f"From: {email_user}\nTo: {email_to}\nSubject: {subject}\n\n{body}"
 
         server.sendmail(email_user, email_to, msg)
 
-        print(G + '\rEmail Sent - {}'.format(i))
-
+        print(G + f'\rEmail Sent - {i}/{total}' + RESET, end='')
         sys.stdout.flush()
 
     server.quit()
 
-    print(R + '\n\n-Proccess Terminated-' + W)
-
+    print(G + '\n\n- Process Terminated -' + RESET)
 
 except KeyboardInterrupt:
-
-    print(R + '\nError - Keyboard Interrupt' + W)
+    print(G + '\nError - Keyboard Interrupt' + RESET)
     sys.exit()
 
 except smtplib.SMTPAuthenticationError:
+    print(G + '\nError - Authentication failed. Check your email/password or allow less secure apps.' + RESET)
+    sys.exit()
 
-    print(R + '\nError - Authentication error, Are you sure the password or the username is correct?' + W)
+except Exception as e:
+    print(G + f'\nUnexpected Error: {e}' + RESET)
     sys.exit()
